3ab410a7d492f84d16806b45fe22ead046418f13
Marcus Vinicius Coelho Guimarães Filho <marcusvnfilho@gmail.com>
2020-06-12 14:17:51 -0300

    fix: update folder nmes

--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -1,4 +1,4 @@
-package com.newproject;
+package com.helloworld;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NewProject";
+    return "HelloWorld";
   }
 }
